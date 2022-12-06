b3b2dda6c030d683c5f45cdb7c5255c38dd38982
Reverendheat <sharp.brandon@gmail.com>
2022-12-06 14:52:52 -0500

    Day 6

---
+++ src/aoc/twotwo/day6/_part2.py
@@ -0,0 +1,25 @@
+from aoc.twotwo.utils.reader import file_reader
+
+def has_duplicates(ci: dict[str,int]) -> bool:
+  for v in ci.values():
+    if v > 1:
+      return True
+  return False
+
+# I know about collections.Counter, just wanted to implement.
+def count_items(char: str, char_list: list[str]) -> dict[str, int]:
+  count_dict = {}
+  for i in char_list:
+    if i not in count_dict:
+      count_dict[i] = 0
+    count_dict[i] += 1
+  return count_dict
+
+def part_two(path: str) -> int | None:
+  for line in file_reader(path):
+    for i in range(len(line) - 1):
+      l = list(line[i:i+14])
+      ci = count_items(line[i], l)
+      if not has_duplicates(ci):
+        print(i+14)
+        return i+14
---
+++ src/aoc/twotwo/day6/main.py
@@ -0,0 +1,7 @@
+from aoc.twotwo.day6._part1 import part_one
+from aoc.twotwo.day6._part2 import part_two
+
+INPUT = "day6/input.txt"
+
+part_one(INPUT)
+part_two(INPUT)
